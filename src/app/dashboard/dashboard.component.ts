30beeba40964f1556028946c692f029247e8cb64
Andy Castle <masterchief_and@live.com>
2025-02-05 15:59:27 -0500

    se configuraron las rutas del proyecto

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
-export class DashboardComponent {}
+export default class DashboardComponent {}
