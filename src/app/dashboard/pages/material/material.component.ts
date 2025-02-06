bfbd75813842cb612981f48b4468c027911beef9
Andy Castle <masterchief_and@live.com>
2025-02-06 15:06:46 -0500

    (no importante): solo se esta viendo como utilizar algunos componentes de angular material

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSlideToggleModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './material.component.html',
 })
 export default class MaterialComponent {}
