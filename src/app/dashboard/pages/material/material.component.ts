48cbf7bdaaaf01072b8f4feed1782b41e8cb2040
Andy Castle <masterchief_and@live.com>
2025-02-06 14:57:06 -0500

    se agrego angular material al proyecto para probar con la version 18

---
+++ src/app/dashboard/pages/material/material.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material',
+  standalone: true,
+  imports: [],
+  templateUrl: './material.component.html',
+})
+export default class MaterialComponent {}
