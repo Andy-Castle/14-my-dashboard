0447c2a52498bdbe38590b322f4c4382df57a20b
Andy Castle <masterchief_and@live.com>
2025-02-06 11:14:22 -0500

    en el defer dentro de los parentesis, se vio como usar el on idle y el on viewport, este ultimo es el mas interesante porque este no es llamado hasta que esta en la vista de la pantalla del usuario

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -4,7 +4,10 @@ import { booleanAttribute, Component, Input, signal } from '@angular/core';
   selector: 'app-title',
   standalone: true,
   imports: [],
-  template: `<h1 class="text-3xl mb-5">{{ title }} - {{ withShadow }}</h1>`,
+  template: `<h1 class="text-3xl mb-5">
+    {{ title }}
+    <!-- - {{ withShadow }} -->
+  </h1>`,
   styles: ``,
 })
 export class TitleComponent {
